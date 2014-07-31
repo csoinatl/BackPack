f77ae2c75d3d93becf3eb5382ffb97e825978d9a
Daniel Kerr <daniel.kerr@ihg.com>
2014-07-31 10:43:16 -0400

    Adjusted minimum spanning tree

--- Project3/Part2/MinimumSpanningTree.java
+++ Project3/Part2/MinimumSpanningTree.java
@@ -7,13 +7,30 @@ import java.util.Queue;
 import Project3.CityData;
 import Project3.CityInfo;
 import Project3.CostEdge;
+import java.util.PriorityQueue;
 
 public class MinimumSpanningTree {
 
 	private CityInfo[] cityInfo;
 	private float[][] costMatrix;
 
-	public CostEdge getMinimumEdge(int startCityCount, CityInfo[] cityInfo,
+	//Opens edges out of a city that has been visisted for the first time
+        public void openCity(PriorityQueue<CostEdge> edgeList, CityInfo startCity, CityInfo[] cityInfo){
+            for(CityInfo connectedCity : cityInfo){
+                int startIndex = CityInfo.CitySet.getCityOrdinal(startCity.getCityName());
+                int endIndex = CityInfo.CitySet.getCityOrdinal(connectedCity.getCityName());
+                if(startIndex != endIndex && costMatrix[startIndex][endIndex] >= 0){
+                    CostEdge newEdge = new CostEdge();
+                    newEdge.setStartCity(startCity);
+                    newEdge.setEndCity(connectedCity);
+                    newEdge.setCost(costMatrix[startIndex][endIndex]);
+                    edgeList.add(newEdge);
+                }
+            }
+           
+        }
+        
+        public CostEdge getMinimumEdge(int startCityCount, CityInfo[] cityInfo,
 			float[][] costMatrix, ArrayList<Integer> cityAddedIn) {
 		CostEdge edgeToReturn = null;
 		float minimum = Long.MAX_VALUE;
@@ -48,41 +65,41 @@ public class MinimumSpanningTree {
 			// Let's traverse the cities and add items
 			String startCity = curCity.getCityName();
 			int startCityCount = CityInfo.CitySet.getCityOrdinal(startCity);
-
+                        CostComparator costComparator = new CostComparator();
+                        PriorityQueue<CostEdge> possibleEdges = new PriorityQueue<CostEdge>(newCityData.getListOfCities().length, costComparator);
 			ArrayList<Integer> cityAddedIn = new ArrayList<Integer>();
 			ArrayList<CostEdge> minimumTreeList = new ArrayList<CostEdge>();
-			cityAddedIn.add(startCityCount);
+			
+                        cityAddedIn.add(startCityCount);
+                        openCity(possibleEdges, curCity, cityInfo);
+                        
+			
 
-			int[] cityList = new int[2];
-			cityList[0] = startCityCount;
-			cityList[1] = -1;
-
-			Queue<Integer> cityQueue = new LinkedList<Integer>();
-
-			int count = 0;
-			while (count < cityInfo.length) {
+			while (cityAddedIn.size() < cityInfo.length) {
 				float minimum = Long.MAX_VALUE;
 				int minimumCity = -1;
 				
-				CostEdge minimumEdge = getMinimumEdge(startCityCount, cityInfo, costMatrix, cityAddedIn);
+				CostEdge minimumEdge = possibleEdges.poll();
 				if (minimumEdge != null 
-						&& minimumEdge.getStartCity().getCityCode() != minimumEdge.getEndCity().getCityCode()) {
+						&& !cityAddedIn.contains(CityInfo.CitySet.getCityOrdinal(minimumEdge.getEndCity().getCityName()))) {
 					minimumTreeList.add(minimumEdge);
 					int endCityCount = CityInfo.CitySet.getCityOrdinal(minimumEdge.getEndCity().getCityCode());
 					cityAddedIn.add(endCityCount);
-					startCityCount = endCityCount;
+					openCity(possibleEdges, minimumEdge.getEndCity(), cityInfo);
 				}
 
-				count++;
 			}
 
 			System.out.println("Printing minimum cost spanning tree from "
 					+ curCity.getCityName());
 
+                        float totalCost = 0.0f;
 			for (CostEdge curCostEdge : minimumTreeList) {
 				System.out.println(curCostEdge.toString());
+                                totalCost += curCostEdge.getCost();
 			}
 
+                        System.out.println("Total cost: " + totalCost);
 			System.out.println("===== End of the tree =====");
 		}
 
